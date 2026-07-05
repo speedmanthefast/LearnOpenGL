15ed9fb3d2c0a4ba5bedf934f4985493c50f32ab
speedmanthefast <jacob@azurelake.net>
2026-07-05 15:38:44 -0500

    5.8.1 complete

--- src/main.cpp
+++ src/main.cpp
@@ -22,14 +22,16 @@ const char *fragmentShaderSource = "#version 330 core\n"
 
 float vertices[] = {
     0.5f, 0.5f, 0.0f, // top right
-    0.5f, -0.5f, 0.0f, // bottom right
-    -0.5f, -0.5f, 0.0f, // bottom left
+    0.75f, -0.5f, 0.0f, // bottom right
+    0.25f, -0.5f, 0.0f,
+    -0.75f, -0.5f, 0.0f, // bottom left
+    -0.25f, -0.5f, 0.0f,
     -0.5f, 0.5f, 0.0f // top left
 };
 
 unsigned int indices[] = {
-    0, 1, 3, // first triangle
-    1, 2, 3 // second triangle
+    0, 1, 2, // first triangle
+    3, 4, 5 // second triangle
 };
 unsigned int EBO;
 
@@ -100,7 +102,7 @@ int main()
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // Tell OpenGL how to interpret vertex data. You are basically describing the layout of your vertex data (which is in the buffer)
-    // The buffer it pulls data from on depends on which VBO is bound to GL_ARRAY_BUFFER
+    // The buffer it pulls data from depends on which VBO is bound to GL_ARRAY_BUFFER
     // In the vertex shader, data pulled from attribute zero will depend on what this function points to
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0); // attribute, size of attribute, data type, normalize t/f, stride, offset
 
@@ -154,6 +156,8 @@ unsigned int compileShader(const char* shaderSource, int type)
     // Create vertex shader
     unsigned int shaderID = glCreateShader(type); // Create shader object
     glShaderSource(shaderID, 1, &shaderSource, NULL);
+
+    // Compile Shader
     glCompileShader(shaderID);
 
     // Check if compilation succeeded
