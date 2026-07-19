763c257b3ab7d4cad5f941807b8128da37d1764e
speedmanthefast <jacob@azurelake.net>
2026-07-19 17:52:00 -0500

    6.5: Color as vertex attributes

--- src/main.cpp
+++ src/main.cpp
@@ -9,29 +9,31 @@
 
 const char *vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
+    "layout (location = 1) in vec3 aColor;\n"
+    "out vec3 ourColor;\n"
     "void main()\n"
     "{\n"
     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "   ourColor = aColor;\n"
     "}\0";
 
 const char *fragmentShaderSource = "#version 330 core\n"
     "out vec4 FragColor;\n"
-    "uniform vec4 ourColor;\n"
+    "in vec3 ourColor;\n"
     "void main()\n"
     "{\n"
-    "   FragColor = ourColor;\n"
+    "   FragColor = vec4(ourColor, 1.0);\n"
     "}\0";
 
 float vertices[] = {
-    0.5f, 0.5f, 0.0f, // top right
-    0.5f, -0.5f, 0.0f, // bottom right
-    -0.5f, -0.5f, 0.0f, // bottom left
-    -0.5f, 0.5f, 0.0f // top left
+    // positions // colors
+    0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom right
+    -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom left
+    0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f // top
 };
 
 unsigned int indices[] = {
-    0, 1, 3, // first triangle
-    1, 2, 3 // second triangle
+    0, 1, 2
 };
 unsigned int EBO;
 
@@ -74,10 +76,14 @@ int main()
     // Tell OpenGL how to interpret vertex data. You are basically describing the layout of your vertex data (which is in the buffer)
     // The buffer it pulls data from on depends on which VBO is bound to GL_ARRAY_BUFFER
     // In the vertex shader, data pulled from attribute zero will depend on what this function points to
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0); // attribute, size of attribute, data type, normalize t/f, stride, offset
+
+    // attribute, size of attribute, data type, normalize t/f, stride, offset
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0); // attribute pointer for vertex data
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float))); // attribute pointer for color data
 
     // Enable attribute
     glEnableVertexAttribArray(0); 
+    glEnableVertexAttribArray(1);
 
     // Unbind buffers
     glBindVertexArray(0);
